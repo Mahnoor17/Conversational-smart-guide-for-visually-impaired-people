80b62f9433d0dd2caea854c006f96628d4bd3c77
Maleeha Khalid <maleehakhalid604@gmail.com>
2020-10-17 14:21:24 +0500

    add object detection module

--- Object detection/Object_detection.py
+++ Object detection/Object_detection.py
@@ -1,2 +1,33 @@
+from imageai.Detection import ObjectDetection
+import cv2
+
 def detectObject(image):
+    """
+    Takes image path as input and returns image with detected object
+    
+    """
+    detector = ObjectDetection()
+
+    model_path = "./models/yolo-tiny.h5"
+    input_path = image
+    output_path = "./output/newimage.jpg"
+
+    detector.setModelTypeAsTinyYOLOv3()
+    detector.setModelPath(model_path)
+    detector.loadModel()
+    detection = detector.detectObjectsFromImage(input_image=input_path, output_image_path=output_path)
+    output=cv2.imread(output_path)
+    cv2.imshow('Detected objects',output)
+    cv2.waitKey(0)
+
+    #probability of detected objects
+    for eachItem in detection:
+        print(eachItem["name"] , " : ", eachItem["percentage_probability"])
+    return output
+
+#calling this function
+output_image=object_detection('./input/dining_table.jpg')
+
+
+
     
