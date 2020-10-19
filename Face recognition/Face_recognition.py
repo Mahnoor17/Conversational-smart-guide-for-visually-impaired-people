480bfb63111735b8fbc0a12ccb46ac433c622392
Mahnoor17 <56605879+Mahnoor17@users.noreply.github.com>
2020-10-19 11:56:47 +0500

    Update Face_recognition.py

--- Face recognition/Face_recognition.py
+++ Face recognition/Face_recognition.py
@@ -4,6 +4,36 @@ from time import sleep
 import os
 import cv2
 
+def registerFace():
+    # For each person, enter one numeric face id
+    face_id = input('\n enter your name ==>  ')
+    print("\n [INFO] Initializing face capture. Look the camera and wait ...")
+    cam = cv2.VideoCapture(0)
+    cam.set(3, 640) # set video width
+    cam.set(4, 480) # set video height
+    face_detector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+    # Initialize individual sampling face count
+    count = 0
+    while(True):
+        ret, img = cam.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_detector.detectMultiScale(gray, 1.3, 5)
+        for (x,y,w,h) in faces:
+            cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)     
+            count += 1
+        # Save the captured image into the datasets folder
+            cv2.imwrite("faces/" + str(face_id) + ".jpg", gray[y:y+h,x:x+w])
+            cv2.imshow('image', img)
+        k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
+        if k == 27:
+            break
+        elif count >= 1: # Take 30 face sample and stop video
+            break
+        cam.release()
+        cv2.destroyAllWindows()
+        
+result=registerFace()
+
 def get_encoded_faces():
     encoded={}
     for dirpath, dnames, fnames in os.walk('./faces'):
@@ -54,10 +84,5 @@ def recognizeFace(image):
         cv2.imshow('Video',img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             return face_names
-
-
-def registerFace(image,rectangle,name):
-    return 0
-
 
 print(recognizeFace('test.jpg'))
