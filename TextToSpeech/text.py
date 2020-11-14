2310eed67e78443dff0235d963d875b0d979d04f
Mahnoor17 <56605879+Mahnoor17@users.noreply.github.com>
2020-11-14 16:49:16 +0500

    Text to speech module

--- TextToSpeech/text.py
+++ TextToSpeech/text.py
@@ -12,22 +12,23 @@ from gtts import gTTS
 # play the converted audio  
   
 from playsound import playsound  
-  
-# It is a text value that we want to convert to audio  
-text_val = input("Enter your text: ")  
-  
-# Here are converting in English Language  
-language = 'en'  
-  
-# Passing the text and language to the engine,  
-# here we have assign slow=False. Which denotes  
-# the module that the transformed audio should  
-# have a high speed  
-obj = gTTS(text=text_val, lang=language, slow=False)  
-  
-#Here we are saving the transformed audio in a mp3 file named  
-# exam.mp3  
-obj.save("file.mp3")  
-  
-# Play the exam.mp3 file  
-playsound("file.mp3")
+def text(text_val):
+    # It is a text value that we want to convert to audio  
+    text_val = input("Enter your text: ")  
+      
+    # Here are converting in English Language  
+    language = 'en'  
+      
+    # Passing the text and language to the engine,  
+    # here we have assign slow=False. Which denotes  
+    # the module that the transformed audio should  
+    # have a high speed  
+    obj = gTTS(text=text_val, lang=language, slow=False)  
+      
+    #Here we are saving the transformed audio in a mp3 file named  
+    # exam.mp3  
+    obj.save("file.mp3")    
+    # Play the exam.mp3 file  
+    playsound("file.mp3")
+    #return text
+result=text(text)
